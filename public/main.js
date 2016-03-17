4ed02814ab482c2d62a336ea0653b18a36092120
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-03-17 19:18:11 -0400

    got username to post on server

---
+++ public/main.js
@@ -0,0 +1,7 @@
+var Backbone = require('backbone');
+var UserFormView = require ('./userFormView');
+var $ = require ('jquery');
+
+$(document).ready(function () {
+  var addUserForm = new UserFormView();
+});
---
+++ public/templates.js
@@ -0,0 +1,8 @@
+module.exports = {
+  userFormTmpl : [
+    '<form class="user-login-form">',
+    '<input type="text" name="username" class="user-login-input" value="Enter Username">',
+    '<button type="button" class ="user-login-button" name="button">SUBMIT</button>',
+    '</form>'
+  ].join(""),
+};
