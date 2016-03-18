f0817d4dc5d88c5f5c77993ccf6b4f2cbf92f0ab
carolineevail <carolineevail@gmail.com>
2016-03-18 16:40:24 -0400

    Parsed csv file and saved each line as an answer object

--- src/main/java/com/theironyard/controllers/MeeSeeksAdviceController.java
+++ src/main/java/com/theironyard/controllers/MeeSeeksAdviceController.java
@@ -1,5 +1,6 @@
 package com.theironyard.controllers;
 
+import com.theironyard.entities.Answer;
 import com.theironyard.entities.Question;
 import com.theironyard.entities.User;
 import com.theironyard.services.AnswerRepository;
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by ericweidman on 3/17/16.
@@ -27,11 +31,23 @@ public class MeeSeeksAdviceController {
     @Autowired
     AnswerRepository answers;
 
+
+
 Server dbui = null;
 
     @PostConstruct
-    public void init() throws SQLException {
+    public void init() throws SQLException, FileNotFoundException {
         dbui = Server.createWebServer().start();
+
+        if (answers.count() == 0) {
+            File f = new File("answers.csv");
+            Scanner scanner = new Scanner(f);
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                Answer answer = new Answer(line);
+                answers.save(answer);
+            }
+        }
     }
 
     @PreDestroy
