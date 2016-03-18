a9c145145bbe7cf34c53f5639aae6a4abe3d9f1a
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-03-17 22:04:56 -0400

    worked on a little styling for homepage.About to start question form

--- public/templates.js
+++ public/templates.js
@@ -1,8 +1,8 @@
 module.exports = {
   userFormTmpl : [
     '<form class="user-login-form">',
-    '<input type="text" name="username" class="user-login-input" value="Enter Username">',
-    '<button type="button" class ="user-login-button" name="button">SUBMIT</button>',
+    '<input type="text" name="username" class="user-login-input form-control" placeholder = "Enter Username"value="">',
+    '<button type="button" class ="user-login-button btn btn-default" name="button">SUBMIT</button>',
     '</form>'
   ].join(""),
 };
