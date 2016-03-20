d9cd3ba932f8c94844f07f0b11ab016a9f3e7f85
ericweidman <ericweidman@gmail.com>
2016-03-20 17:25:58 -0400

    Fixed edit route

--- src/main/java/com/theironyard/controllers/MeeSeeksAdviceController.java
+++ src/main/java/com/theironyard/controllers/MeeSeeksAdviceController.java
@@ -89,8 +89,8 @@ public class MeeSeeksAdviceController {
     }
 
     @RequestMapping(path = "/question", method = RequestMethod.PUT)
-    public Question editQuestion(@RequestBody Question question, Answer answer) {
-        answer.setAnswer("TEST TEXT");
+    public Question editQuestion(@RequestBody Question question) {
+        question.setAnswer(getRandomAnswer());
         questions.save(question);
         return question;
     }
